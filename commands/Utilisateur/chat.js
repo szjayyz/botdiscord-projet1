ca18bd820340568406586e09a94807fd2acc15dc
Dj-pon <alexis.ollandfr@gmail.com>
2019-03-25 17:39:54 +0100

    Fichiers

---
+++ commands/Utilisateur/chat.js
@@ -0,0 +1,35 @@
+const Command = require("../../modules/Command.js");
+const { get } = require("snekfetch");
+
+class Chat extends Command {
+  constructor(client) {
+    super(client, {
+      name: "chat",
+      description: "Une image de chat.",
+      usage: "chat",
+      category: "Animaux"
+    });
+  }
+
+  async run(message) {
+    const { body } = await get("https://api.thecatapi.com/v1/images/search");
+    message.delete();
+
+    await message.channel.send({
+      embed: {
+        title: "Si l'image ne se charge pas, cliquez ici.",
+        url: body[0].url,
+        color: 6192321,
+        image: {
+          url: body[0].url
+        },
+        footer: {
+          icon_url: message.author.avatarURL,
+          text: `${message.author.tag}`
+        }
+      }
+    });
+  }
+}
+
+module.exports = Chat;
